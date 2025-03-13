adc7169074dd473ff8aca89b0ba85689c701b9f4
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-03-13 21:31:06 +0600

    add file...

---
+++ app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,8 @@
+import {NextAuthOptions} from 'next-auth'
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export default function options() {
+  return (
+    <div>options</div>
+  )
+}
